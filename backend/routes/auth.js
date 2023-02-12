17a03ecb109da6f71190e79b2cc57f6ae6a20b4b
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-13 01:36:03 +0530

    authenticate user using Post and bcrypt.copare and jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -22,29 +22,72 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
 
-    const salt = await bcrypt.genSalt(10);
-    const secPass = await bcrypt.hash(req.body.password, salt);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res
-        .status(400)
-        .json({ error: "Sorry a user with this email already exists" });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occurred");
     }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: secPass,
-    });
+  }
+);
 
-    const data = {
-      user: {
-        id: user.id,
-      },
-    };
-    const authToken = jwt.sign(data, JWT_SECRET);
-    res.json({ authToken });
+//Authenticate a User using: POST "/api/auth/login". Doesn't require auth
+router.post(
+  "/login",
+  body("email", "Please enter a valid Email").isEmail(),
+  body("password", "Password cannot be blank").exists(),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const passCompare = await bcrypt.compare(password, user.password);
+      if (!passCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occurred");
+    }
   }
 );
 
