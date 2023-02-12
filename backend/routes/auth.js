5d141b22773947ebf6541609c14cb5369f140a44
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-12 23:31:00 +0530

    used async await in the creation of user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,26 +3,34 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-//Create a User using: POST "/api/auth/". Doesn't require auth
+//Create a User using: POST "/api/auth/createUser". Doesn't require auth
 router.post(
-  "/",
+  "/createUser",
   body("name", "Name must be of atleast 3 characters").isLength({ min: 3 }),
   body("email", "Please enter a valid Email").isEmail(),
   // password must be at least 5 chars long
   body("password", "Password must be of atleast 5 characters").isLength({
     min: 5,
   }),
-  (req, res) => {
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "Sorry a user with this email already exists" });
+    }
+    user = await User.create({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-    }).then((user) => res.json(user));
+    });
+    res.json(user);
   }
 );
 
