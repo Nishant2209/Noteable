c87466e2aeca2aa922747bb26d6aa27a8fa5b776
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-12 20:53:49 +0530

    added routes for notes and authentication

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const connectToMongo = require('./server');
-const express = require('express')
+const connectToMongo = require("./server");
+const express = require("express");
 connectToMongo();
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello bitches !!')
-})
+app.use("/api/auth", require(`./routes/auth`));
+app.use("/api/notes", require(`./routes/notes`));
+app.get("/", (req, res) => {
+  res.send("hello bitches !!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
