6efba18cc41644fc708b501875f0955ab7a9b03d
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-23 19:29:11 +0530

    made a component to add a note

---
+++ src/components/AddNotes.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+export const AddNotes = () => {
+  const context = useContext(noteContext);
+  const { addNote } = context;
+  const [note, setNote] = useState({
+    title: "",
+    description: "",
+    tag: "default",
+  });
+  const handleClick = (e) => {
+    e.preventDefault();
+    addNote(note.title, note.description, note.tag);
+  };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container my-3">
+      <h1>Add a Note</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="title" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            name="title"
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="text"
+            rows={4}
+            className="form-control"
+            id="description"
+            name="description"
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="tag" className="form-label">
+            Tag
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tag"
+            name="tag"
+            onChange={onChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
