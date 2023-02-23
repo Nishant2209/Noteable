db13e1e62a8d54457fc00757a868eee148f087dd
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-23 20:46:02 +0530

    added functionality to delete and edit icon

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
 
 export default function NoteItem(props) {
+  const context = useContext(noteContext);
   const { note } = props;
+  const { deleteNote, editNote } = context;
   return (
     <div className="col-md-3">
       <div className="card my-3" style={{ width: "18rem" }} key={note.id}>
@@ -9,8 +12,18 @@ export default function NoteItem(props) {
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
           <p className="card-text text-info">{note.tag}</p>
-          <i className="fa-solid fa-trash mx-2 text-danger"></i>
-          <i className="fa-solid fa-pen-to-square mx-2"></i>
+          <i
+            className="fa-solid fa-trash mx-2 text-danger"
+            onClick={() => {
+              deleteNote(note._id);
+            }}
+          ></i>
+          <i
+            className="fa-solid fa-pen-to-square mx-2"
+            onClick={() => {
+              editNote(note._id, note.title, note.description, note.tag);
+            }}
+          ></i>
         </div>
       </div>
     </div>
