b0d89201fda211c8c2caaba75e3dbc0e228280f2
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-23 19:30:22 +0530

    added note tag and delete and edit icons

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -7,9 +7,10 @@ export default function NoteItem(props) {
       <div className="card my-3" style={{ width: "18rem" }} key={note.id}>
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">
-            {note.description}
-          </p>
+          <p className="card-text">{note.description}</p>
+          <p className="card-text text-info">{note.tag}</p>
+          <i className="fa-solid fa-trash mx-2 text-danger"></i>
+          <i className="fa-solid fa-pen-to-square mx-2"></i>
         </div>
       </div>
     </div>
