774b7acbe0e67916a29bc1cd9d23973036dd46e5
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-23 12:19:21 +0530

    added  navbar, home, aboyt component

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+
+export default function About() {
+  const a = useContext(noteContext);
+  return <div>This is about {a.name}</div>;
+}
