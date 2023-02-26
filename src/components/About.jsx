a31f9ac13d785c102dabec906e125fd4c3ba60d0
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-26 13:32:32 +0530

    updated about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { useContext } from "react";
-import noteContext from "../context/notes/noteContext";
 
 export default function About() {
-  const a = useContext(noteContext);
-  return <div>This is about {a.name}</div>;
+  return <div>Coming Soon</div>;
 }
