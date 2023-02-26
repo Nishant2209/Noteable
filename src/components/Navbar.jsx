51c25228f04497d2f7290602aba13a297e9323c3
Nishant Kumar Singh <nishant220902@gmail.com>
2023-02-26 12:26:15 +0530

    added logout functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
   return (
     <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
@@ -14,7 +19,7 @@ export default function Navbar() {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="/navbarSupportedContent"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -45,14 +50,30 @@ export default function Navbar() {
               </Link>
             </li>
           </ul>
-          <form className="d-flex gap-3" role="search">
-            <Link className="btn btn-outline-light" role="button" to="/login">
-              Login
-            </Link>
-            <Link className="btn btn-light" role="button" to="/signup">
-              Sign Up
-            </Link>
-          </form>
+          {!localStorage.getItem("token") ? (
+            <form className="d-flex gap-3" role="search">
+              <Link className="btn btn-outline-light" role="button" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-light" role="button" to="/signup">
+                Sign Up
+              </Link>
+            </form>
+          ) : (
+            <div className="d-flex align-items-center">
+              <Link to="/userdetails" className="mx-3">
+                <i className="fa-solid fa-user text-light border rounded-circle p-2 border-3"></i>
+              </Link>
+              <Link
+                className="btn btn-light"
+                role="button"
+                to="/login"
+                onClick={handleLogout}
+              >
+                LogOut
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
